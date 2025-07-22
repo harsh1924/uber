5a0811a5a6dd28a5f16ba059113e538d3ad6b256
harsh1924 <harshrpanwar@gmail.com>
2025-07-22 14:01:46 +0530

    Frontend Started

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import './index.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import UserLogin from "./pages/UserLogin";
+import UserSignup from "./pages/UserSignup";
+import CaptainLogin from "./pages/CaptainLogin";
+import CaptainSignup from "./pages/CaptainSignup";
+
+const App = () => {
+  return (
+    <div>
+      {/* Define your routes here */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        {/* User */}
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+
+        {/* Captain */}
+        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-signup" element={<CaptainSignup />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import '../index.css'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="h-screen w-full flex justify-between flex-col bg-red-400">
+      <img className='' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+        <div className="bg-white">
+          <h2>Get Started with Uber</h2>
+          <button>Continue</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
