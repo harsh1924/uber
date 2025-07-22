2837fb2260ea06c3e8ae8dbeee339fdc2fb64078
harsh1924 <harshrpanwar@gmail.com>
2025-07-22 15:08:18 +0530

    Completed Landing Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import '../index.css'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
-      <div className="h-screen w-full flex justify-between flex-col bg-red-400">
-      <img className='' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
-        <div className="bg-white">
-          <h2>Get Started with Uber</h2>
-          <button>Continue</button>
+      <div className="bg-[url(https://plus.unsplash.com/premium_photo-1731842686156-74895c29a87b?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dHJhZmZpYyUyMGxpZ2h0fGVufDB8fDB8fHww)] bg-cover bg-center h-screen pt-8 w-full flex justify-between flex-col bg-red-400">
+        <img className='w-16 ml-9' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+        <div className="bg-white pb-7 p-4">
+          <h2 className='text-3xl font-bold'>
+            Get Started with Uber
+          </h2>
+          <Link
+            to="/login"
+            className='w-full flex items-center justify-center bg-black text-white py-3 rounded-md mt-5'
+          >
+            Continue
+          </Link>
         </div>
       </div>
     </div>
--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
-
 const UserLogin = () => {
   return (
-    <div>UserLogin</div>
+    <div className="p-7 font-serif">
+      <form action="">
+        <h3 className="text-xl mb-2">What's your email address?</h3>
+        <input
+          className="bg-[#eeeeee] rounded px-4 py-2 w-full text-lg placeholder:text-sm mb-7"
+          type="email"
+          placeholder="Enter your email address..."
+          required
+          id="email"
+        />
+
+        <h3 className="text-xl mb-2">What's your password?</h3>
+        <input
+          className="bg-[#eeeeee] rounded px-4 py-2 w-full text-lg placeholder:text-sm mb-7"
+          type="password"
+          placeholder="Enter your password..."
+          required
+          id="password"
+        />
+
+        <button className="bg-[#111] text-[#fff] font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-sm mb-7">
+          Login
+        </button>
+      </form>
+    </div>
   )
 }
 
